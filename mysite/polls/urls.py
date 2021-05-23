978bb37fdcd5dcbe651a74cae1b37ae7c9a02b43
William Watson <williambwatson42@gmail.com>
2021-05-23 16:41:28 +0100

    added templates for index.html and detail.html, including a form for questions and their choices

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -2,11 +2,13 @@ from django.contrib import admin
 from django.urls import path, include
 from . import views
 
+app_name = 'polls'
+
 urlpatterns = [
     #polls/
     path('', views.index, name='index'),
     #polls/5/detail/
-    path('<int:question_id>/detail/', views.detail, name='detail'),
+    path('<int:question_id>/', views.detail, name='detail'),
     #polls/5/results/
     path('<int:question_id>/results/', views.results, name='results'),
     #polls/5/vote/
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Hello, World.')
+    latest_question_list = Question.objects.order_by('-pub_date')
+    data = { 'latest_question_list': latest_question_list  }
+    return render(request, 'polls/index.html', data)
 
 def detail(request, question_id):
-    return HttpResponse(f'You\'re looking at question {question_id}')
+    question = Question.objects.get(id=question_id)
+    data = { 'question': question }
+    return render(request, 'polls/detail.html', data)
 
 def results(request, question_id):
     return HttpResponse(f'Results page for question {question_id}')
